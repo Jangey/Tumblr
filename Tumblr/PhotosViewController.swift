e0624df3c94387635727548c8b4bc00d53750a8a
Jangey <jangey.lu@gmail.com>
2018-09-05 19:30:57 -0700

    MileStone3.1

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var posts: [[String: Any]] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         fetchPhoto()
     }
@@ -40,6 +46,17 @@ class PhotosViewController: UIViewController {
         }
         task.resume()
 
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
